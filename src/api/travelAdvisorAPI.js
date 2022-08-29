70d11270bc54326723924ac4eec789f6716e8958
madhuy1260 <madhu@rsaamerica.com>
2022-08-29 17:19:31 +0530

    google maps -rapidapi

--- src/api/travelAdvisorAPI.js
+++ src/api/travelAdvisorAPI.js
@@ -1,41 +1,54 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable consistent-return */
-import axios from 'axios';
+/* eslint-disable quotes */
+import axios from "axios";
 
+// eslint-disable-next-line consistent-return
 export const getPlacesData = async (type, sw, ne) => {
   try {
-    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
-      params: {
-        bl_latitude: sw.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-        tr_latitude: ne.lat,
-      },
-      headers: {
-        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
-        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-      },
-    });
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+          "x-RapidAPI-Key":
+            "3b210d74abmshe7621fe7f84bcabp1744c4jsn9b92d48e7cc8",
+          "x-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
 
     return data;
   } catch (error) {
-    console.log(error);
+    // console.log(error);
   }
 };
 
 export const getWeatherData = async (lat, lng) => {
   try {
     if (lat && lng) {
-      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
-        params: { lat, lon: lng },
-        headers: {
-          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
-          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-        },
-      });
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: { lat, lon: lng },
+          headers: {
+            "x-RapidAPI-Key":
+              "3b210d74abmshe7621fe7f84bcabp1744c4jsn9b92d48e7cc8",
+            "x-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
+          },
+        }
+      );
 
       return data;
     }
   } catch (error) {
-    console.log(error);
+    // console.log(error);
   }
 };
